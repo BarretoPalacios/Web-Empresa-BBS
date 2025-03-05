efcbd6d3fef5b714d62e62696c3d16311b099399
David Barreto <david11barreto11@gmail.com>
2025-03-05 18:49:41 -0500

    cambio en el scroll de las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,43 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import NotFound from "./pages/NotFound.jsx";
 import MorePage from "./pages/MorePage.jsx";
 import ServicePage from "./pages/ServicePage.jsx";
 import ProjectPage from "./pages/ProjectPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-  {
-    path: "/services",
-    element: <ServicePage />,
-  },
-  {
-    path: "/projects",
-    element: <ProjectPage />,
-  },
-  {
-    path: "/more",
-    element: <MorePage />,
-  },
-]);
+// Componente para asegurar que la página se desplace al inicio al cambiar de ruta
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+// Componente principal que define las rutas
+function Root() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop /> {/* Asegura que la página se desplace al inicio al cambiar de ruta */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/services" element={<ServicePage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/more" element={<MorePage />} />
+        <Route path="*" element={<NotFound />} /> {/* Ruta comodín para páginas no encontradas */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+// Renderizado de la aplicación
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <RouterProvider router={router} />
+    <Root />
   </StrictMode>
 );
